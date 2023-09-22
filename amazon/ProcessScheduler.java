46b6bcf33a5b36409ca45b03c4f742a1f7be0af5
TAIsRich <Richard.lyu0120@gmail.com>
2023-09-21 23:22:44 -0700

    add comments for processScheduler and readChapter

--- amazon/ProcessScheduler.java
+++ amazon/ProcessScheduler.java
@@ -6,23 +6,28 @@ public class ProcessScheduler {
         int n = priorities.length;
         List<Process> processes = new ArrayList<>();
 
+        // 初始化：将所有进程按顺序添加到列表中
         for (int i = 0; i < n; i++) {
             processes.add(new Process(i, priorities[i]));
         }
 
+        // 循环直到算法终止条件满足
         while (true) {
             Map<Integer, Integer> priorityCount = new HashMap<>();
+            // 统计每个优先级的进程数量
             for (Process p : processes) {
                 priorityCount.put(p.priority, priorityCount.getOrDefault(p.priority, 0) + 1);
             }
 
             int maxSharedPriority = 0;
+            // 找出至少有两个进程共享的最大优先级
             for (Map.Entry<Integer, Integer> entry : priorityCount.entrySet()) {
                 if (entry.getValue() >= 2 && entry.getKey() > maxSharedPriority) {
                     maxSharedPriority = entry.getKey();
                 }
             }
 
+            // 如果没有这样的优先级，终止算法
             if (maxSharedPriority == 0) break;
 
             int countOfMaxPrioritySeen = 0;
@@ -30,9 +35,9 @@ public class ProcessScheduler {
             while (iterator.hasNext()) {
                 Process p = iterator.next();
                 if (p.priority == maxSharedPriority) {
-                    if (countOfMaxPrioritySeen == 0) { // Remove the first process
+                    if (countOfMaxPrioritySeen == 0) { // 删除第一个进程
                         iterator.remove();
-                    } else if (countOfMaxPrioritySeen == 1) { // Reduce the priority of the second process
+                    } else if (countOfMaxPrioritySeen == 1) { // 减少第二个进程的优先级
                         p.priority /= 2;
                     }
                     countOfMaxPrioritySeen++;
@@ -41,12 +46,14 @@ public class ProcessScheduler {
         }
 
         int[] result = new int[processes.size()];
+        // 提取剩余进程的优先级
         for (int i = 0; i < processes.size(); i++) {
             result[i] = processes.get(i).priority;
         }
         return result;
     }
 
+    // 定义进程类，包括索引和优先级
     static class Process {
         int index;
         int priority;
@@ -57,7 +64,6 @@ public class ProcessScheduler {
         }
     }
 
-
     public static void main(String[] args) {
         // int[] priorities = {6, 6, 6, 1, 2, 2};
         // int[] priorities = {5, 3, 5, 4, 2};
@@ -66,4 +72,16 @@ public class ProcessScheduler {
         System.out.println(Arrays.toString(result));  // Output: [3, 6, 0]
     }
 }
+/**
+ * 
+ * Several processes are scheduled for execution on an AWS server
+On one server, n processes are scheduled where the th process is assigneda priority of priorityli. The processes are placed sequentially in a queue andare numbered 1, 2, .., n. The server schedules the processes per thefollowing algorithm:
+1. It finds the maximum priority shared by at least 2 processes. Let that bedenoted by p.
+1. If there is no such priority or p = 0, the algorithm is terminated.2. Otherwise, select the two of the processes with the lowest indexesand priority p, call them process1 and process2.
+3. The server executes process1 and removes it from the queue.
+4. To avoid starvation, it reduces the priority of process2 to floorp/2).
+5. Start again from step 1.
+Given the initial priority of the processes, find the final priority of theprocesses which remain after the algorithm terminates.
+Note that relative the arrangement of remaining processes in the queueremains the same, only their priorities change.
+ */
 
--- amazon/readChapter.java
+++ amazon/readChapter.java
@@ -1,10 +1,19 @@
+// 这个类表示“读章节”相关的方法
 public class readChapter {
+    public static void main(String[] args) {
+        System.out.println(getMinPages(new int[]{5, 3, 4}, 4));
+        System.out.println(getMinPages(new int[]{2, 3, 4}, 4));
+    }
+
+    // 获取读完所有章节所需的最小页面数，以确保在给定的天数内完成
     public static int getMinPages(int[] pages, int days) {
         int start = 0, end = 0;
+        // 找到章节中的最大页数
         for (int page : pages) {
             end = Math.max(end, page);
         }
 
+        // 使用二分查找方法来查找最佳的阅读速度
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
             if (getPage(pages, mid) <= days) {
@@ -20,11 +29,13 @@ public class readChapter {
         if (getPage(pages, end) <= days) {
             return end;
         }
-        return -1;
+        return -1;  // 如果找不到合适的阅读速度，则返回-1
     }
 
+    // 计算给定速度下，完成所有章节所需的天数
     private static int getPage(int[] pages, double speed) {
         int days = 0;
+        // 遍历每一个章节，并累计所需的天数
         for (int page : pages) {
             days += Math.ceil(page / speed);
         }
